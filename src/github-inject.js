ca3497880a04ece50b4ea4d085fb89b66975aafe
Igal Tabachnik <hmemcpy@gmail.com>
2015-03-25 12:49:54 +0200

    First run

---
+++ src/github-inject.js
@@ -0,0 +1,9 @@
+// Please make this better, send a PR! :)
+
+$('.octicon-logo-github').on('click', function () {
+     jQuery(".header-logo-wordmark").attr('href', 'http://github.com');
+});
+
+$('.octicon-logo-gist').on('click', function () {
+     jQuery(".header-logo-wordmark").attr('href', 'http://gist.github.com');
+});
